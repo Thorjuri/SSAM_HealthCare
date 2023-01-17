fd7b2cecea5a33d0f4de3695ab77f1168af3d1f0
Thorjuri <sjl1992@naver.com>
2023-01-17 22:49:08 +0900

    1/17 evaluations 운동평가 조회 및 식이평가 API 작성 완료

--- services/evaluationsService.js
+++ services/evaluationsService.js
@@ -1,9 +1,11 @@
 const EvaluationsRepository = require('../repositories/evaluationsRepository');
 const UsersRepository = require('../repositories/usersRepository');
+const ContentsRepository = require('../repositories/contentsRepository');
 
 class EvaluationsService {
     evaluationsRepository = new EvaluationsRepository();
     usersRepository = new UsersRepository();
+    contentsRepository = new ContentsRepository();
 
     createExer = async(nickname, disease, activity, height, 
                         weight, waist, hip, glucose, SBP, DBP)=> {
@@ -24,8 +26,7 @@ class EvaluationsService {
                 featureCode = ''
             };
 
-
-            if(userInfo.age >= 65) {
+            if(userInfo.age >= 65) { //시니어가 비만보다 우선
                 senior = true
                 featureCode = 'sr'
             }else {
@@ -67,8 +68,33 @@ class EvaluationsService {
         return data;
     };
 
+    getExerResult = async(nickname)=> {
+        const { statusCode } = await this.evaluationsRepository.getEvaluations(nickname);
+        const contents = await this.contentsRepository.getContents(statusCode);
+        return { statusCode, contents };
+    };
 
+    dietEvaluations = async(nickname, intake)=> {
+        const { BMR, activity } = await this.evaluationsRepository.getEvaluations(nickname);
+        const RDI = 0;  //일일권장섭취량(Recommended Daily Intake)
+        if(activity === 1){
+            RDI = BMR * 1.2  + (BMR * 0.1);  //activity에 따른 활동계수 연산
+        }else if(activity === 2){
+            RDI = BMR * 1.3 + (BMR * 0.1);
+        }else if(activity === 3){
+            RDI = BMR * 1.5 + (BMR * 0.1);
+        }else if(activity === 4){
+            RDI = BMR * 1.75 + (BMR * 0.1);
+        };
 
+        if(intake >= (RDI*1.1)){
+            return { RDI, message: "일일권장섭취량의 10% 이상 초과하였습니다. 일일 섭취 열량을 줄이세요." }
+        }else if(intake < (RDI*0.9)){
+            return { RDI, message: "일일권장섭취량 대비 10% 이상 부족합니다. 섭취량을 보충해주세요." }
+        }else {
+            return { RDI, message: "섭취량이 적절합니다. 현재 식사량을 유지해주세요." }
+        };
+    };
 };
 
 module.exports = EvaluationsService;
